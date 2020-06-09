da3d0db3541ca641f959b7b17631c90529da79c5
AlexisPapaioannou <48021731+AlexisPapaioannou@users.noreply.github.com>
2020-06-09 15:16:34 +0300

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,4 +4,4 @@ from sklearn.metrics import accuracy_score, f1_score
 
 def prediction_evaluation(prediction, y_test):
     print('Test accuracy is {}'.format(accuracy_score(y_test, prediction)))
-    print('Test F1 score is {}'.format(f1_score(y_test, prediction, average='weighted')))
+    print('Test F1 score is {}'.format(f1_score(y_test, prediction)))
